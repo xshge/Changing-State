128f7f9af6dc8a7bb4c61deefdbc9d82feace575
xshenge <xshen12138@outlook.com>
2025-02-27 00:29:57 -0500

    change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    base: process.env.NODE_ENV === 'production' ? '/Changing-State/dist/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/Changing-State/' : ''
 });
