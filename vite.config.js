9b6906bc3f3e46714b77d43d0be962d8e634ec89
xshenge <xshen12138@outlook.com>
2025-02-27 00:35:42 -0500

    config build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    base: process.env.NODE_ENV === 'production' ? '/Changing-State/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/Changing-State/' : '',
+    build: {
+        outDir: 'dist',
+    }
 });
